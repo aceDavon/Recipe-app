b2e94870578dd9dc34b5f160e20f804099670b6d
aceDavon <makojidavid@gmail.com>
2022-12-16 10:23:25 +0100

    Add styling to user component
    Style the user component to reflect proper wireframe experience

--- db/migrate/20221213095122_create_recipe_foods.rb
+++ db/migrate/20221213095122_create_recipe_foods.rb
@@ -4,8 +4,6 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.string :quantity
-      t.belongs_to :food
-      t.belongs_to :recipe
 
       t.timestamps
     end
